65347ba53d86b5857b59b208047199cea28bf373
Arshad <shaikarshadali01@gmail.com>
2025-05-04 16:44:25 +0530

    code

--- src/creational/prototype/ProtoTypePattern.java
+++ src/creational/prototype/ProtoTypePattern.java
@@ -3,10 +3,10 @@ package src.creational.prototype;
 class ProtoTypePattern{
     public static void main(String[] args) {
 
-        Address address=new Address("Kadiri","kadiri");
+        Address address=new Address("City","place");
         User user1=new User("Arshad","email","0123456789","leetcode",1,address);
         User user2=user1.clone();
-        user2.getAddress().setCity("Anantapur");
+        user2.getAddress().setCity("city2");
         System.out.println(user1.toString());
         System.out.println(user2.toString());
     }
