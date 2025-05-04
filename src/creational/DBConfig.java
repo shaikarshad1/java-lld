f1e303f45acdaf718bb6f4aa654f0d3420e317ea
Arshad <shaikarshadali01@gmail.com>
2025-05-04 13:13:01 +0530

    code

---
+++ src/creational/DBConfig.java
@@ -0,0 +1,19 @@
+package src.creational;
+
+public class DBConfig {
+
+    private static DBConfig dbConnection= null;
+
+    private DBConfig(){
+        System.out.println("obtained db connection");
+    }
+
+    public static DBConfig getDbConnection(){
+        if(dbConnection==null)
+            synchronized (DBConfig.class) {
+            if(dbConnection==null)
+                dbConnection = new DBConfig();
+            }
+        return dbConnection;
+    }
+}
---
+++ src/creational/Singleton.java
@@ -0,0 +1,28 @@
+package src.creational;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Singleton {
+
+    public static void main(String[] args) {
+//       eagerLoading();
+        lazyLoading();
+    }
+
+    public static void eagerLoading(){
+        DBConfig dbConnection1=DBConfig.getDbConnection();
+        DBConfig dbConnection2=DBConfig.getDbConnection();
+        System.out.println(dbConnection1);
+        System.out.println(dbConnection2);
+    }
+
+    public static void lazyLoading(){
+        ExecutorService es= Executors.newCachedThreadPool();
+        es.execute(()-> System.out.println(DBConfig.getDbConnection()));
+        es.execute(()-> System.out.println(DBConfig.getDbConnection()));
+        es.execute(()-> System.out.println(DBConfig.getDbConnection()));
+        es.shutdown();
+    }
+
+}
